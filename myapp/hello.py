aeac77db654157917c1703fd07b2242e9c02b3b4
larusx <larusx@163.com>
2013-04-16 19:28:30 +0800

    upload

--- myapp/hello.py
+++ myapp/hello.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello(): 
 	return render_template('a.html')
 
+@app.route('/storage',methods=['GET','POST'])
+def upload_file():
+	if request.method== 'POST':
+		f = request.files['file1']
+		f.save('/usr/upload'+secure_filename(f.filename))
+	return 'OK'
 if __name__ == '__main__': 
 	app.run(debug=True)
