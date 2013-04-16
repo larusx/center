89f53e7f3223b060faa643b21d36758552c9e900
larusx <larusx@163.com>
2013-04-16 21:22:00 +0800

    modify

--- myapp/hello.py
+++ myapp/hello.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request,send_from_directory
 from werkzeug import secure_filename
 app = Flask(__name__) 
 
-UPLOAD_FOLDER='/usr/upload'
+UPLOAD_FOLDER='/usr/upload/'
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 
 @app.route('/') 
@@ -18,7 +18,7 @@ def hello():
 def upload_file():
 	if request.method == 'POST':
 		f = request.files['file1']
-		f.save('/usr/upload'+secure_filename(f.filename))
+		f.save(UPLOAD_FOLDER+secure_filename(f.filename))
 	return 'OK'
 
 @app.route('/upload/<filename>')
