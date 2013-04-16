94d528669fa5e5f76b6b3fd0fe4570bc80587643
larusx <larusx@163.com>
2013-04-16 20:14:03 +0800

    storage :

--- myapp/hello.py
+++ myapp/hello.py
@@ -12,7 +12,7 @@ def hel():
 def upload_file():
 	if request.method== 'POST':
 		f = request.files['file1']
-		f.save('/usr/center/venv/center/myapp/static/upload/'+secure_filename(f.filename))
+		f.save('/usr/center/venv/center/myapp/static/upload/a')
 	return render_template('a.html')
 if __name__ == '__main__': 
 	app.run(debug=True)
