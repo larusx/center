938d36c7406707ff9e1ef88b8d4c1bb9b9c81862
larusx <larusx@163.com>
2013-04-16 20:32:38 +0800

    mod

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,18 +1,21 @@
-from flask import Flask,render_template
+import os
+from flask import Flask,render_template,request
 from werkzeug import secure_filename
 app = Flask(__name__) 
 
 @app.route('/') 
 def hello(): 
 	return render_template('a.html')
+
 @app.route('/hello')
 def hel():
 	return 'hello'
+
 @app.route('/storage',methods=['GET','POST'])
 def upload_file():
-	if request.method== 'POST':
+	if request.method == 'POST':
 		f = request.files['file1']
-		f.save('/usr/center/venv/center/myapp/static/upload/a')
+		f.save('/usr/center/venv/center/myapp/upload/'+secure_filename(f.filename))
 	return render_template('a.html')
 if __name__ == '__main__': 
 	app.run(debug=True)
