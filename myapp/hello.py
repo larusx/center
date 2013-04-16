9e5a49e98ba0d2572d299b2cba2cdfb27a95ebf4
larusx <larusx@163.com>
2013-04-16 19:48:12 +0800

    modify upload

--- myapp/hello.py
+++ myapp/hello.py
@@ -10,7 +10,7 @@ def hello():
 def upload_file():
 	if request.method== 'POST':
 		f = request.files['file1']
-		f.save('/usr/upload/'+secure_filename(f.filename))
+		f.save('/usr/center/venv/center/myapp/static/upload/'+secure_filename(f.filename))
 	return render_template('a.html')
 if __name__ == '__main__': 
 	app.run(debug=True)
