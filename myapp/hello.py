f2fc38c2365f0b7f6e3b69f3c292d9c8eec9322f
larusx <larusx@163.com>
2013-04-24 12:35:37 +0800

    search

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask,render_template,request,send_from_directory,redirect,url_for,flash
 from werkzeug import secure_filename
 app = Flask(__name__) 
@@ -24,5 +25,15 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
+@app.route('/search',methods=['POST'])
+def search_result():
+    if request.method == 'POST':
+        search_content=re.compile(request.form['search_content'].encode('utf-8'))
+        return_list=[] 
+        for root,dirs,files in os.walk(UPLOAD_FOLDER):
+            for fn in files:
+                if search_content.search(fn):
+                    return_list.append(fn.decode('utf-8'))
+    return render_template('a.html',return_list=return_list)
 if __name__ == '__main__': 
     app.run()
