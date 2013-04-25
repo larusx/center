e41894fd0d30c20a36da11efeb86be7c4590bd3b
root <root@localhost.(none)>
2013-04-25 02:58:39 -0400

    multi file upload ok

--- myapp/hello.py
+++ myapp/hello.py
@@ -19,11 +19,12 @@ def hello():
 @app.route('/upload',methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
-        flist = request.files['file1']
-	for f in flist:
+        flist = request.files
+	for f in flist.getlist('file1'):
 	    if os.path.exists(UPLOAD_FOLDER+f.filename.encode('utf-8')):
-    	        return render_template('a.html',status='''<div class="row"><div class="span4 offset6"><div class="alert alert-success">File exists!!!</div></div></div>''')
+    	        return render_template('a.html',status='''<div class="row"><div class="span4 offset6"><div class="alert alert-success">%s exists!!!</div></div></div>''' % f.filename)
             f.save(UPLOAD_FOLDER+f.filename.encode('utf-8'))
+	    
     return render_template('a.html',status='''<div class="row"><div class="span4 offset6"><div class="alert alert-success">Upload Success!!!</div></div></div>''')
 @app.route('/upload/<filename>')
 def uploaded_file(filename):
