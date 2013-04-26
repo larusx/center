02d84c834fab6b00637ab79098e88546f9990e04
larusx <larusx@163.com>
2013-04-26 16:57:15 +0800

    ajax

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,48 +1,61 @@
 import os
 import re
-from flask import Flask,render_template,request,send_from_directory,redirect,url_for,flash
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for, flash, jsonify
 from werkzeug import secure_filename
-app = Flask(__name__) 
-app.secret_key=os.urandom(24)
-UPLOAD_FOLDER='/usr/upload/'
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
-app.debug=True
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+UPLOAD_FOLDER = '/usr/upload/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.debug = True
+
+
 @app.route('/')
 def hello():
-    #return_list=[]
-    #for root,dirs,files in os.walk(UPLOAD_FOLDER):
+    # return_list=[]
+    # for root,dirs,files in os.walk(UPLOAD_FOLDER):
     #    for fn in files:
     #        return_list.append(fn.decode('utf-8'))
-    #return render_template('a.html',return_list=return_list)
+    # return render_template('a.html',return_list=return_list)
     return render_template('index.html')
 
-@app.route('/upload',methods=['GET','POST'])
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         flist = request.files
-    exist_list=[]
-    success_list=[]
+    exist_list = []
+    success_list = []
     for f in flist.getlist('file1'):
-        file_name=f.filename.encode('utf-8')
+        file_name = f.filename.encode('utf-8')
         if os.path.exists(UPLOAD_FOLDER+file_name):
             exist_list.append(file_name.decode('utf-8'))
             continue
         f.save(UPLOAD_FOLDER+file_name)
         success_list.append(file_name.decode('utf-8'))
-    return render_template('upload_return.html',exist_list=exist_list,success_list=success_list)
+    return render_template('upload_return.html', exist_list=exist_list, success_list=success_list)
+
+
 @app.route('/upload/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/search',methods=['POST','GET'])
+
+@app.route('/add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+
+@app.route('/search', methods=['POST', 'GET'])
 def search_result():
-    return_list=[] 
+    return_list = []
     if request.method == 'POST':
-        search_content=request.form['search_content'].encode('utf-8')
-        for root,dirs,files in os.walk(UPLOAD_FOLDER):
+        search_content = request.form['search_content'].encode('utf-8')
+        for root, dirs, files in os.walk(UPLOAD_FOLDER):
             for fn in files:
-                if re.search(search_content,fn,re.IGNORECASE):
+                if re.search(search_content, fn, re.IGNORECASE):
                     return_list.append(fn.decode('utf-8'))
-    return render_template('a.html',return_list=return_list)
-if __name__ == '__main__': 
+    return render_template('a.html', return_list=return_list)
+if __name__ == '__main__':
     app.run()
